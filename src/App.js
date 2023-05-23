f3bd2ae53c5d0b1ec3ca52991afd95106a3b7e6d
Sean Siganoff <nationwideclassics@yahoo.com>
2023-05-23 16:22:08 -0400

    added grocerys

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Expense Tracker App</h1>
+        <h4>Grocerys:</h4>
       </header>
     </div>
   );
